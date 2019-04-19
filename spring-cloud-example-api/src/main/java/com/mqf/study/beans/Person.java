b01a354a9eace20831aa6534cd74be924c161447
李洪智 <1160948933@qq.com>
2019-04-19 14:08:49 +0800

    update

--- spring-cloud-example-api/src/main/java/com/mqf/study/beans/Person.java
+++ spring-cloud-example-api/src/main/java/com/mqf/study/beans/Person.java
@@ -1,5 +1,10 @@
 package com.mqf.study.beans;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
 import java.io.Serializable;
 
 /**
@@ -8,6 +13,11 @@ import java.io.Serializable;
  * @Author mqf
  * @Date 2019/4/15 17:05
  */
+@SuppressWarnings("serial")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)
 public class Person implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -35,67 +45,4 @@ public class Person implements Serializable {
      * 数据库
      */
     private String dbSource;
-
-
-
-    public Person() {
-    }
-    
-
-    public Person(Long id, String name, Integer age, String email, String dbSource) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.email = email;
-        this.dbSource = dbSource;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
-    public String getDbSource() {
-        return dbSource;
-    }
-
-    public void setDbSource(String dbSource) {
-        this.dbSource = dbSource;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", email='" + email + '\'' +
-                ", dbSource='" + dbSource + '\'' +
-                '}';
-    }
 }
---
+++ spring-cloud-example-api/src/main/java/com/mqf/study/beans/Person3.java
@@ -0,0 +1,101 @@
+package com.mqf.study.beans;
+
+import java.io.Serializable;
+
+/**
+ * @ClassName Person3
+ * @Description TODO
+ * @Author mqf
+ * @Date 2019/4/19 14:02
+ */
+public class Person3 implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键ID
+     */
+    private Long id;
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 年龄
+     */
+    private Integer age;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 数据库
+     */
+    private String dbSource;
+
+
+
+    public Person3() {
+    }
+
+
+    public Person3(Long id, String name, Integer age, String email, String dbSource) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.dbSource = dbSource;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getDbSource() {
+        return dbSource;
+    }
+
+    public void setDbSource(String dbSource) {
+        this.dbSource = dbSource;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", dbSource='" + dbSource + '\'' +
+                '}';
+    }
+}
