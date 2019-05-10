2155ddee8431d744f3eddbaed8b2d8baaa67cd3b
李洪智 <1160948933@qq.com>
2019-05-10 15:55:15 +0800

    zuul filter 异常信息返回

--- spring-cloud-example-zuul-gateway-9527/src/main/java/com/mqf/study/config/ZuulConfig.java
+++ spring-cloud-example-zuul-gateway-9527/src/main/java/com/mqf/study/config/ZuulConfig.java
@@ -1,11 +1,15 @@
 package com.mqf.study.config;
 
 import com.mqf.study.filter.AccessFilter;
+import com.mqf.study.filter.AccessFilter_2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @ClassName ZuulConfig
+ * AccessFilter 与 AccessFilter_2 二选一
+ *  AccessFilter 无 error JSOn 返回
+ *  AccessFilter_2 有 error JSOn 返回，但是，此处就返回一次，再次请求就会返回正常的数据
  * @Description TODO
  * @Author mqf
  * @Date 2019/5/9 15:48
@@ -17,4 +21,9 @@ public class ZuulConfig {
     public AccessFilter accessFilter(){
         return new AccessFilter();
     }
+
+//    @Bean
+//    public AccessFilter_2 accessFilter_2(){
+//        return new AccessFilter_2();
+//    }
 }
